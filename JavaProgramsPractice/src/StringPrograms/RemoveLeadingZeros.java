eb56902a19e62d23392dc8f793263f39bd1a021a
yashwanthrao425@gmail.com <yashwanthrao425@gmail.com>
2024-04-24 20:11:49 +0530

    test

--- JavaProgramsPractice/src/StringPrograms/RemoveLeadingZeros.java
+++ JavaProgramsPractice/src/StringPrograms/RemoveLeadingZeros.java
@@ -3,13 +3,7 @@ package StringPrograms;
 public class RemoveLeadingZeros {
 
 
-	public static methodOne(){
-
-		//this is the dummy code added to delete
-
-	}
-
-	public static void main(String[] args) {
+	public static void wayOneMethod() {
 		String str = "0000002347";
 		String newstr = "";
 		int index = 0;
@@ -26,6 +20,32 @@ public class RemoveLeadingZeros {
 		newstr = str.substring(index, str.length());
 
 		System.out.println(newstr);
+		
+	}
+
+	
+	public static void wayTwoMethod() {
+		String str = "0000002347";
+		String newstr = "";
+		int index = 0;
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (c != '0') {
+				index = i;
+				break;
+
+			}
+
+		}
+
+		newstr = str.substring(index, str.length());
+
+		System.out.println(newstr);
+		
+	}
+	public static void main(String[] args) {
+		wayOneMethod();
+		wayTwoMethod();
 
 	}
 }
