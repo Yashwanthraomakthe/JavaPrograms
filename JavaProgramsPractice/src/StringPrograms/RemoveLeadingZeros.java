ed995cde85a2d427f6721badefaad08467c1cc65
yashwanthrao425@gmail.com <yashwanthrao425@gmail.com>
2024-04-24 20:13:21 +0530

    test2

--- JavaProgramsPractice/src/StringPrograms/RemoveLeadingZeros.java
+++ JavaProgramsPractice/src/StringPrograms/RemoveLeadingZeros.java
@@ -3,7 +3,7 @@ package StringPrograms;
 public class RemoveLeadingZeros {	
 
 	public static void main(String[] args) {
-		String str = "0000002347";
+		String str = "0000002347892341212";
 		String newstr = "";
 		int index = 0;
 		for (int i = 0; i < str.length(); i++) {
